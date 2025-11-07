8cc92cde3fec680adb90d70ad1f30a6a5d9501d8
ManongKayl <leavebroke77@gmail.com>
2025-11-07 22:31:00 +0800

    Revert the changes accidently by one member earlier
    AYAW MO PATAKA OG CHANGE2 SA MAIN BRANCH!

--- teambot-esp32-01/database_setup.sql
+++ teambot-esp32-01/database_setup.sql
@@ -11,3 +11,10 @@ CREATE TABLE rfid_reg (
     rfid_data VARCHAR(255) PRIMARY KEY,
     rfid_status BOOLEAN NOT NULL
 );
+
+insert into rfid_reg (rfid_data, rfid_status) values
+('04A3B2C1D4E5', TRUE),
+('1234567890AB', TRUE),
+('0987654321CD', FALSE),
+('FE6C0004', TRUE),
+('23EE9014', FALSE);
