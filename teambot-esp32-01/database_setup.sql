af6e129ff800153968611a5d99eb993d18a62a72
Jusmeeyo <160337464+Jusmeeyo@users.noreply.github.com>
2025-11-29 13:44:58 +0800

    Mao nani
    Wala nakoy time sa computeran kyle
    
    Co-Authored-By: ManongKayl <172178587+ManongKayl@users.noreply.github.com>

--- teambot-esp32-01/database_setup.sql
+++ teambot-esp32-01/database_setup.sql
@@ -1,12 +1,15 @@
 CREATE DATABASE IF NOT EXISTS it414_db_teambot;
 USE it414_db_teambot;
 
+"Mao ni diri makita ang logs"
 CREATE TABLE rfid_logs (
     time_log DATETIME NOT NULL,
     rfid_data VARCHAR(255) NOT NULL,
     rfid_status BOOLEAN NOT NULL
 );
 
+
+"Mao ni para sa registered RFID"
 CREATE TABLE rfid_reg (
     rfid_data VARCHAR(255) PRIMARY KEY,
     rfid_status BOOLEAN NOT NULL
